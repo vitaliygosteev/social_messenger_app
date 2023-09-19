242c3763f315a184857681d6e47310e4a0dfaeeb
Vitaliy <vitaliygosteev@gmail.com>
2023-09-19 17:16:05 +0400

    feat: create application layout

---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import { usePathname } from 'next/navigation';
+import { signOut } from 'next-auth/react';
+import { HiChat } from 'react-icons/hi';
+import { HiUsers, HiArrowLeftOnRectangle } from 'react-icons/hi2';
+
+import useConversation from './useConversations';
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: 'Chat',
+        href: '/conversatins',
+        icon: HiChat,
+        active: pathname === '/conversations' || !!conversationId,
+      },
+      {
+        label: 'Users',
+        href: '/users',
+        icon: HiUsers,
+        active: pathname === '/users',
+      },
+      {
+        label: 'Logout',
+        href: '#',
+        icon: HiArrowLeftOnRectangle,
+        onClick: () => signOut(),
+      },
+    ],
+    [pathname, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
